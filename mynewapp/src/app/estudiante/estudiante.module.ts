6f268a940cd60d1d981d5a1932d22f3c96134cbd
Jorge Solorzano <j.solorzanod@uniandes.edu.co>
2024-10-28 10:34:34 -0500

    modulo estudiante

---
+++ mynewapp/src/app/estudiante/estudiante.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EstudianteComponent } from './estudiante.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [EstudianteComponent]
+})
+export class EstudianteModule { }
